d9bf645432906bf1e9686ff5cb0e8b5425e9e67f
Alexander Barthel <albar965@mailbox.org>
2024-03-06 14:47:43 +0100

    Settings::logMessages()

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 /*****************************************************************************
-* Copyright 2015-2020 Alexander Barthel alex@littlenavmap.org
+* Copyright 2015-2024 Alexander Barthel alex@littlenavmap.org
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -101,6 +101,7 @@ PLUGIN_API int XPluginStart(char *outName, char *outSig, char *outDesc)
 
   // Initialize logging and force logfiles into the system or user temp directory
   LoggingHandler::initializeForTemp(Settings::getOverloadedPath(":/littlexpconnect/resources/config/logging.cfg"));
+  Settings::logMessages();
   LoggingUtil::logSystemInformation();
   LoggingUtil::logStandardPaths();
 
